2c4bd098f547ede782bd800aa10c3cfcfac6ca32
Rahhhulll <rkrahulrk651@gmail.com>
2026-09-15 18:55:17 +0530

    Implement JWT authentication and RBAC

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const { registerUser, loginUser } = require('../controllers/auth.controller');
+
+const authMiddleware = require('../middleware/auth.middleware');
+
+const authorizeRoles = require('../middleware/role.middleware');
+
+const router = express.Router();
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+
+router.get('/profile', authMiddleware, (req, res) => {
+    res.status(200).json({
+        message: 'Profile accessed successfully',
+        user: req.user
+    });
+});
+
+router.get('/admin', authMiddleware, authorizeRoles('admin'), (req, res) => {
+    res.status(200).json({
+        message: 'Welcome Admin',
+        user: req.user
+    });
+});
+
+router.get('/mentor', authMiddleware, authorizeRoles('mentor'), (req, res) => {
+    res.status(200).json({
+        message: 'Welcome Mentor',
+        user: req.user
+    });
+});
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth.routes');
 
 const app = express();
 
@@ -8,6 +9,8 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
     res.json({
